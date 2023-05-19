77a6103fe3e8335dc43d3561116b4f3075704a67
esoume <esoume@yahoo.ca>
2023-05-19 14:02:51 -0400

    add onesignal sdk in app for push notificationb

--- app/src/main/java/com/esoume/coding/weatherapp/WeatherApp.kt
+++ app/src/main/java/com/esoume/coding/weatherapp/WeatherApp.kt
@@ -3,5 +3,26 @@ package com.esoume.coding.weatherapp
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+
+import com.onesignal.OneSignal
+
+// Replace the below with your own ONESIGNAL_APP_ID
+const val ONESIGNAL_APP_ID = "3ee64a9e-3f1f-47a1-bcef-87ffa68e43e5"
+
 @HiltAndroidApp
-class WeatherApp() : Application()
+class WeatherApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Logging set to help debug issues, remove before releasing your app.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this)
+        OneSignal.setAppId(ONESIGNAL_APP_ID)
+
+        // promptForPushNotifications will show the native Android notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
+        OneSignal.promptForPushNotifications()
+    }
+}
